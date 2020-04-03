6305c5e8127565658f50b73e1dedde920194b48c
delvalle-serch <62078463+delvalle-serch@users.noreply.github.com>
2020-04-02 23:25:34 -0600

    1
    1

---
+++ java.js
@@ -0,0 +1,7 @@
+if (screen.width < 400) {
+	document.write('<link rel="stylesheet" type="text/css" href="css/est_400.css">');
+}
+if else (screen.width > 400) {
+	document.write('<link rel="stylesheet" type="text/css" href="css/est.css">');
+}
+alert("hola");
